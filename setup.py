28db23ade8fa2881703a3143ed8725b2266aa023
Jimmy Shen <jimmy@tsu.co>
2015-06-19 17:37:41 -0400

    added setuptools script

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='sunset',
+    version='0.1',
+    packages=['sunset'],
+    author='Jimmy Shen',
+    author_email='thejimmyshen@gmail.com',
+    description='Use comments to add expiration dates to your code.',
+    license='MIT',
+    install_requires=[
+    ],
+    url='https://github.com/jimmyshen/sunset',
+    download_url='https://github.com/jimmyshen/sunset/tarball/0.1',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: MIT License'
+    ]
+)
